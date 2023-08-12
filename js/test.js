c80be693f0cd78a7bb165b4ee07f14fde30256f6
Neo <speldipn@gmail.com>
2023-08-12 10:40:08 +0900

    test2

---
+++ js/test.js
@@ -0,0 +1,6 @@
+function clickTest() {
+    var anchor = document.createElement("a");
+    anchor.href = "https://smartstore.naver.com/kyochon1991/products/8946332045";
+    anchor.target = "_blank";
+    anchor.click();
+}
